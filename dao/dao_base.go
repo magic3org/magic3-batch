3b6529c947722d6157b89aea42470007a2e21099
naoki hirata <naoki@magic3.org>
2016-10-31 13:01:26 +0900

    DAOベースを更新。

--- dao/dao_base.go
+++ dao/dao_base.go
@@ -30,11 +30,11 @@ func Destroy() error {
 }
 
 /*
-機能: クエリーを実行し１行取得
+機能: クエリーを実行しMapで１行取得
 	array: 実行クエリー
 	params: クエリー埋め込み用パラメータ
 	row: Map化したレコード
-	err: 終了ステータス
+	err: 終了ステータス(nil=取得できたとき,nil以外=取得できなかったとき)
 */
 func selectRecord(query string, params ...interface{}) (row map[string]interface{}, err error) {
 	var mapRow = make(map[string]interface{})
@@ -44,28 +44,43 @@ func selectRecord(query string, params ...interface{}) (row map[string]interface
 	}
 	defer rows.Close()
 
+	// カラム名取得
 	colNames, err := rows.Columns()
 	if err != nil {
 		return nil, err
 	}
 
-	cols := make([]interface{}, len(colNames))
-	colPtrs := make([]interface{}, len(colNames))
-	for i := 0; i < len(colNames); i++ {
-		colPtrs[i] = &cols[i]
+	// データ取得領域確保
+	values := make([]sql.RawBytes, len(colNames))
+
+	// データ取得用のパラメータ作成
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
 	}
 
 	for rows.Next() {
-		err = rows.Scan(colPtrs...)
+		// 1レコード取得
+		err = rows.Scan(scanArgs...)
 		if err != nil {
 			return nil, err
 		}
-		for i, col := range cols {
-			mapRow[colNames[i]] = col
+
+		// 文字列に変換してMapに格納
+		var value string
+		for i, colValue := range values {
+			// Here we can check if the value is nil (NULL value)
+			if colValue == nil {
+				value = "NULL"
+			} else {
+				value = string(colValue)
+			}
+			mapRow[colNames[i]] = value
 		}
 		break
 	}
 
+	// SELECT結果が1行もない場合はErrNoRowsを返す
 	if len(mapRow) == 0 {
 		return nil, sql.ErrNoRows
 	}
